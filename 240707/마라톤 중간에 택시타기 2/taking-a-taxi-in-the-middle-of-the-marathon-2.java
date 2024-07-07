a9d395c8073944a33ea83ccf2913cae018c41d34
SSiik <sik_5571@naver.com>
2024-07-07 23:44:11 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240707/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+
+    public static int taxi(Info a,Info b){
+        return Math.abs(a.x-b.x) + Math.abs(a.y-b.y);
+
+    }
+
+    static int minn = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N;
+        N = Integer.parseInt(br.readLine());
+        Info[] arr = new Info[N];
+
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i] = new Info(a,b);
+        }
+
+
+        
+        int startFlag = 1;
+        while(startFlag < N-1 ){
+            int sum = 0;
+            Info cur = new Info(arr[0].x,arr[0].y);
+            for(int j=1;j<N-1;j++){ 
+                if(startFlag == j) continue; //첫번째와 마지막 체크포인트는 제외해야함
+                sum += taxi(cur,arr[j]);
+                cur=arr[j];
+            }
+            startFlag++;
+            sum += taxi(cur,arr[N-1]); //마지막쪽 거리는 수동으로 더해줍니다.
+            minn = Math.min(minn,sum);
+        }
+
+        
+        
+        System.out.println(minn);
+
+    }
+
+    static class Info{
+
+        int x;
+        int y;
+
+        Info(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
